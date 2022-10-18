b85a7bcf4af218c4404aa358a0f4b4ff8a1e4c1f
Behnam <behnam.amiri@gitkraken.com>
2022-10-18 13:19:58 -0700

    Add shopping list window

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const url = require('url');
 const path = require('path');
 
 const { app, BrowserWindow, Menu } = electron;
+
 let mainWindow;
+let addWindow; 
 
 app.on('ready', function() {
     // Create new window 
@@ -14,20 +16,35 @@ app.on('ready', function() {
         pathname: path.join(__dirname, 'mainWindow.html'),
         protocol: 'file:',
         slashes: true
-    }));
+    })); // => file://dirname/mainWindow.html
 
     // Build menu
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
     Menu.setApplicationMenu(mainMenu);
-}); // => file://dirname/mainWindow.html
+});
 
+const createAddWindow = () => {
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add Shopping List Item'
+    });
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'addWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+}
 
 const mainMenuTemplate = [
     {
         label: 'File',
         submenu: [
             {
-                label: 'Add Item'
+                label: 'Add Item',
+                click() {
+                    createAddWindow();
+                }
             },
             {
                 label: 'Clear Items'
