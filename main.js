5a7cc32a840050bc55a224d2ab06c094841da884
Behnam <behnam.amiri@gitkraken.com>
2022-10-19 08:37:20 -0700

    Clear item

--- main.js
+++ main.js
@@ -56,8 +56,6 @@ const createAddWindow = () => {
 }
 
 ipcMain.on('item:add', function(e, item) {
-    console.log('Hello');
-    console.log(item);
     mainWindow.webContents.send('item:add', item);
     addWindow.close();
 });
@@ -73,7 +71,10 @@ const mainMenuTemplate = [
                 }
             },
             {
-                label: 'Clear Items'
+                label: 'Clear Items',
+                click() {
+                    mainWindow.webContents.send('item:clear');
+                }
             },
             {
                 label: 'Quit',
